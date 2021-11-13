a53588fcddf08f2b343ef03bd75c10622eec559c
Auteotl <93289809+Auteotl@users.noreply.github.com>
2021-11-13 08:54:09 +0300

    Удаление старой папки

--- javaFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.maybelater {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.maybelater to javafx.fxml;
-    exports com.example.maybelater;
-}
