dd188f8ad26148368f6537346b6a9d44e0cfd810
Auteotl <93289809+Auteotl@users.noreply.github.com>
2021-11-13 08:58:59 +0300

    Создание новой папки с проектом на FX

---
+++ maybeLater/src/main/java/com/example/maybelater/HelloApplication.java
@@ -0,0 +1,24 @@
+package com.example.maybelater;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 920, 630);
+        stage.setTitle("maybeLater");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ maybeLater/src/main/java/com/example/maybelater/HelloController.java
@@ -0,0 +1,42 @@
+package com.example.maybelater;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button buttonEditCat;
+
+    @FXML
+    private ChoiceBox<?> catedorField;
+
+    @FXML
+    private Button delButton;
+
+    @FXML
+    private TextField descriptionField;
+
+    @FXML
+    private DialogPane dialogPaneEdit;
+
+    @FXML
+    private MenuItem menuEditCat;
+
+    @FXML
+    private TextField textEditCat;
+
+    @FXML
+    private Button urlButton;
+
+    @FXML
+    private TextField urlField;
+
+}
